354bbc9f0180fbe83288c8c04650a9238f8c29f7
CommandTM <commandnoobmc23@gmail.com>
2023-11-19 16:28:02 -0500

    Initial Push

---
+++ HaikuAP/HaikuAP/Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using On;
+
+namespace HaikuAP;
+
+[BepInPlugin("haiku.apclient", "Haiku AP Client", "0.5")]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"HI, IT'S COMMAND!");
+    }
+}
---
+++ HaikuAP/HaikuAP/obj/Debug/net35/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace HaikuAP
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "HaikuAP";
+        public const string PLUGIN_NAME = "HaikuAP";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
