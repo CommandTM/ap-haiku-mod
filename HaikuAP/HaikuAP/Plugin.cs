9947ce104f69d2a4201c9dd896f9bc42075b9b88
CommandTM <commandnoobmc23@gmail.com>
2023-11-27 17:56:29 -0500

    Doing Some Funny gitnore

--- HaikuAP/HaikuAP/Plugin.cs
+++ HaikuAP/HaikuAP/Plugin.cs
@@ -8,8 +8,13 @@ public class Plugin : BaseUnityPlugin
     private void Awake()
     {
         // Plugin startup logic
-        Logger.LogInfo($"AW-03=AW-01 Is A Useless Entrance");
-        Logger.LogInfo($"TLB-T=TLB-03 Is A Useless Entrance");
-        Logger.LogInfo($"IB-01=AW-02 Is A Useless Entrance");
+        Logger.LogError($"AW-03=AW-01 Is A Useless Entrance");
+        Logger.LogError($"TLB-T=TLB-03 Is A Useless Entrance");
+        Logger.LogError($"IB-01=AW-02 Is A Useless Entrance");
+        Logger.LogError($"SW-TW=SW-01 Is A Useless Entrance");
+        Logger.LogError($"CC-01=CC-QP Is A Useless Entrance");
+        Logger.LogError($"CC-04=CC-01 Is A Useless Entrance");
+        
+        Logger.LogError($"CC-QP Is A Useless Region");
     }
 }
--- HaikuAP/HaikuAP/obj/Debug/net35/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace HaikuAP
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "HaikuAP";
-        public const string PLUGIN_NAME = "HaikuAP";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
