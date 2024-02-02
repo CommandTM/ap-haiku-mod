9e3d37a1e6b4d031632d9a83271dc60c0e92c55d
CommandTM <commandnoobmc23@gmail.com>
2024-02-02 00:05:35 -0500

    Haiku Is Connecting

---
+++ HaikuAP/HaikuAP/APPlugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Archipelago.MultiClient.Net;
+using BepInEx;
+
+namespace HaikuAP
+{
+    [BepInPlugin("haiku.apclient", "Haiku AP Client", "0.5")]
+    [BepInDependency("haiku.mapi", "1.0.1.0")]
+    [BepInDependency("com.bepis.bepinex.configurationmanager", "18.1")]
+    public sealed class APPlugin : BaseUnityPlugin
+    {
+        public static ArchipelagoSession apSession;
+        public static LoginResult apResult;
+        public void Awake()
+        {
+            BepInEx.Logging.Logger.Sources.Add(Settings.apConnection);
+            // Plugin startup logic
+            Logger.LogError($"AW-03=AW-01 Is A Useless Entrance");
+            Logger.LogError($"TLB-T=TLB-03 Is A Useless Entrance");
+            Logger.LogError($"IB-01=AW-02 Is A Useless Entrance");
+            Logger.LogError($"SW-TW=SW-01 Is A Useless Entrance");
+            Logger.LogError($"CC-01=CC-QP Is A Useless Entrance");
+            Logger.LogError($"CC-04=CC-01 Is A Useless Entrance");
+            Logger.LogFatal($"CC-QP Is A Useless Region");
+            
+            Settings.Init(Config);
+        }
+    }
+}
---
+++ HaikuAP/HaikuAP/Settings.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Archipelago.MultiClient.Net;
+using Archipelago.MultiClient.Net.Enums;
+using BepInEx.Configuration;
+using BepInEx;
+using BepInEx.Logging;
+using Modding;
+using UnityEngine;
+using Logger = HarmonyLib.Tools.Logger;
+
+namespace HaikuAP
+{
+    class Settings
+    {
+        public static ConfigEntry<string> apIP { get; private set; }
+        public static ConfigEntry<string> apPort { get; private set; }
+        public static ConfigEntry<string> apSlotName { get; private set; }
+        public static ConfigEntry<bool> apUsePassword { get; private set; }
+        public static ConfigEntry<string> apPassword { get; private set; }
+
+        private const string archipelago = "AP";
+        public static ManualLogSource apConnection = new ManualLogSource("AP Connection");
+        
+        public static void Init(ConfigFile config)
+        {
+            apIP = config.Bind(archipelago, "Archipelago IP", "archipelago.gg", 
+                "IP address used to connect");
+            apPort = config.Bind(archipelago, "Archipelago Port", "38281", 
+                "Port used to connect");
+            apSlotName = config.Bind(archipelago, "Archipelago Slot", "Player", 
+                "Slot to connect to");
+            apUsePassword = config.Bind(archipelago, "Use Password?", false,
+                "Should the client connect with a password?");
+            apPassword = config.Bind(archipelago, "Archipelago Password", "", 
+                "The password to use if enabled");
+            
+            ConfigManagerUtil.createButton(config, Connect, archipelago, "Connect", 
+                "Connect to the Multiworld");
+            
+            config.Save();
+        }
+    
+        private static void Connect()
+        {
+            if (!apUsePassword.Value)
+            {
+                apPassword.Value = null;
+            }
+            
+            APPlugin.apSession = ArchipelagoSessionFactory.CreateSession(/*apIP.Value, Int32.Parse(apPort.Value)*/new Uri("unspecified://archipelago.gg:61611"));
+            APPlugin.apResult = APPlugin.apSession.TryConnectAndLogin(
+                "Haiku, The Robot",
+                apSlotName.Value,
+                ItemsHandlingFlags.AllItems,
+                new Version(0, 4, 2),
+                null,
+                null,
+                apPassword.Value,
+                false);
+
+            if (!APPlugin.apResult.Successful)
+            {
+                LoginFailure fail = (LoginFailure)APPlugin.apResult;
+                foreach (string error in fail.Errors)
+                {
+                    apConnection.LogFatal(error);
+                }
+            }
+        }
+    }
+}
+
