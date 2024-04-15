b6d480173c81301626f064c3a4db20defff895a1
colin-every <commandnoobmc23@gmail.com>
2024-04-15 09:42:25 -0400

    deps!

--- HaikuAP/HaikuAP/APPlugin.cs
+++ HaikuAP/HaikuAP/APPlugin.cs
@@ -13,16 +13,8 @@ namespace HaikuAP
         public static LoginResult apResult;
         public void Awake()
         {
+            Logger.LogInfo("AP Haiku Loaded");
             BepInEx.Logging.Logger.Sources.Add(Settings.apConnection);
-            // Plugin startup logic
-            Logger.LogError($"AW-03=AW-01 Is A Useless Entrance");
-            Logger.LogError($"TLB-T=TLB-03 Is A Useless Entrance");
-            Logger.LogError($"IB-01=AW-02 Is A Useless Entrance");
-            Logger.LogError($"SW-TW=SW-01 Is A Useless Entrance");
-            Logger.LogError($"CC-01=CC-QP Is A Useless Entrance");
-            Logger.LogError($"CC-04=CC-01 Is A Useless Entrance");
-            Logger.LogFatal($"CC-QP Is A Useless Region");
-            
             Settings.Init(Config);
         }
     }
