c73ad5845fcaf9f7856db9087790380d57a34566
colin-every <commandnoobmc23@gmail.com>
2024-04-29 10:22:16 -0400

    0_0
    I Never Want To Modify Assemblies Again

---
+++ HaikuAP/HaikuAP/SaveHijacker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace HaikuAP;
+
+public class SaveHijacker
+{
+    public static void Hijack()
+    {
+        On.PCSaveManager.Save += _saveAPThings;
+    }
+
+    private static void _saveAPThings(On.PCSaveManager.orig_Save orig, PCSaveManager self, string filepath)
+    {
+        orig(self, filepath);
+        self.es3SaveFile.Save<String>("test", "Test");
+    }
+}
